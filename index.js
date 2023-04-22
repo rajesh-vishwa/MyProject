cf76a7f916fc6a280a5e65ec2bcfe138206b7b70
RV <sms.rajesh@gmail.com>
2023-04-22 09:30:33 +0530

    ReactNativeGestureHandler added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWithGesture = () => (
+  <GestureHandlerRootView>
+    <App />
+  </GestureHandlerRootView>
+);
+
+AppRegistry.registerComponent(appName, () => AppWithGesture);
